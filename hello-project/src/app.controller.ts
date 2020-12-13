fe4c054166ebd1c06f038d4dcaef616d4c3d305e
nika mosayebi <nikibeh@yahoo.com>
2020-12-13 18:21:30 +0330

    Add entities & relations and establish a connection with the database

--- hello-project/src/app.controller.ts
+++ hello-project/src/app.controller.ts
@@ -5,13 +5,4 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("/hi")
- getHello(): string {
- return this.appService.getHello();
- }
- @Get("/bye")
- getBye(): string {
- return this.appService.getHello();
- } 
-
 }
--- hello-project/src/app.module.ts
+++ hello-project/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
