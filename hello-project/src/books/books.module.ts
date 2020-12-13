07a3394e10a4515ff85dd9182f2ffe8b0d2b0396
nika mosayebi <nikibeh@yahoo.com>
2020-12-13 18:27:58 +0330

    Genrate module, service, controller and add user DTOs

--- hello-project/src/books/books.module.ts
+++ hello-project/src/books/books.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
 
-@Module({})
+@Module({
+  providers: [BooksService],
+  controllers: [BooksController]
+})
 export class BooksModule {}
