f2be2b5e8a33eea97225d938a4eea310079b532d
nika mosayebi <nikibeh@yahoo.com>
2020-12-13 19:56:55 +0330

    Complete controllers, services & modules, move DTOs

--- hello-project/src/books/books.controller.ts
+++ hello-project/src/books/books.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import BooksService from './books.service';
+import CreateGenreDto from './dto/create-book.dto';
 
-@Controller('books')
-export class BooksController {}
+@Controller('genre')
+export default class GenreController {
+  constructor(private readonly booksService: BooksService) {}
+  @Post('post')
+  postGenre( @Body() genre: CreateGenreDto) {
+    return this.booksService.insert(genre);
+  }
+  @Get()
+  getAll() {
+    return this.booksService.getAllBooks();
+  }
+}
